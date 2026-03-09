830d6abfc8dda6720cff72c7cae6c1146745bb1f
ANSH CHIMANIA <anshchim.work@gmail.com>
2026-03-09 17:49:17 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://version2-jri3.onrender.com',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' }
 });
